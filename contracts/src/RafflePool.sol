e39b59bd3d50290b91ea9606b5ad3eb99b029252
Josh <joshhellings@outlook.com>
2023-08-15 23:23:20 +0100

    Add Twabs functionality & integrate pickWinner

--- contracts/src/RafflePool.sol
+++ contracts/src/RafflePool.sol
@@ -55,6 +55,12 @@ contract RafflePool is VRFConsumerBaseV2 {
         CALCULATING // Waiting for Chainlink VRF
     }
 
+    // track TWABs for each user, represents a user's balance at a particular time
+    struct Twab {
+        uint256 balance;
+        uint256 timestamp;
+    }
+
     ///////////////////
     // State Variables
     ///////////////////
@@ -67,6 +73,10 @@ contract RafflePool is VRFConsumerBaseV2 {
     address[] private s_players;
     mapping(address => uint256) private s_userDeposit;
     mapping(address => uint256) private s_playerIndex;
+
+    // map user address to TWABs array, to be updated with each transaction
+    mapping(address => Twab[]) private s_userTwabs;
+    Twab[] private s_totalDepositTwabs; // array for tracking total deposit balance TWAB over time
 
     // @dev Duration of the raffle in seconds
     uint256 private immutable i_interval;
@@ -136,6 +146,11 @@ contract RafflePool is VRFConsumerBaseV2 {
         // Update truthful staking rewards total
         s_stakingRewardsTotal = newBalance - s_totalUserDeposits;
         _addPlayer(msg.sender);
+
+        // Add a new Twab to the user's array of Twabs
+        s_userTwabs[msg.sender].push(Twab(s_userDeposit[msg.sender], uint32(block.timestamp)));
+        // Push total supply observations
+        s_totalDepositTwabs.push(Twab(s_totalUserDeposits, uint32(block.timestamp)));
     }
 
     // Following Checks-Effects-Interactions
@@ -146,6 +161,8 @@ contract RafflePool is VRFConsumerBaseV2 {
         s_stakingRewardsTotal = totalBalance() - s_totalUserDeposits;
         s_userDeposit[msg.sender] += amountStEth;
         s_totalUserDeposits += amountStEth;
+        s_userTwabs[msg.sender].push(Twab(s_userDeposit[msg.sender], uint32(block.timestamp)));
+        s_totalDepositTwabs.push(Twab(s_totalUserDeposits, uint32(block.timestamp)));
         _addPlayer(msg.sender);
 
         i_stETH.permit(msg.sender, address(this), amountStEth, deadline, v, r, s);
@@ -162,7 +179,9 @@ contract RafflePool is VRFConsumerBaseV2 {
         s_stakingRewardsTotal = totalBalance() - s_totalUserDeposits;
         s_userDeposit[msg.sender] -= amount;
         s_totalUserDeposits -= amount;
-        _removePlayer(msg.sender);
+        s_userTwabs[msg.sender].push(Twab(s_userDeposit[msg.sender], uint32(block.timestamp)));
+        s_totalDepositTwabs.push(Twab(s_totalUserDeposits, uint32(block.timestamp)));
+        _removePlayer(msg.sender); // remove player if they withdraw all their stETH
 
         // Transfer stETH from this contract to the user
         bool success = i_stETH.transfer(msg.sender, amount);
@@ -208,12 +227,25 @@ contract RafflePool is VRFConsumerBaseV2 {
         // 1. Calculate the sum of all TWABs for the time period of interest (`totalTWAB`).
         // 2. Scale the random number from Chainlink VRF to the range `[0, totalTWAB]` using modulo (`%`). This will give us a "ticket number" in the virtual array.
         // 3. Iterate over users, summing their TWABs, until the sum exceeds the ticket number. The current user is the winner.
-        uint256 scaledNumber = randomWords[0] % s_players.length;
 
+        uint256 totalTwab = calculateTwab(address(0), s_lastTimestamp, block.timestamp);
+        uint256 scaledNumber = randomWords[0] % totalTwab;
+        // Initialise a running total of TWABs
+        uint256 runningTotal = 0;
+        address winner;
+        for (uint256 i = 0; i < s_players.length; i++) {
+            runningTotal += calculateTwab(s_players[i], s_lastTimestamp, block.timestamp);
+            // If the running total exceeds the ticket number, this user is the winner
+            if (runningTotal > scaledNumber) {
+                winner = s_players[i];
+            }
+        }
+        // Update the last timestamp
+        s_lastTimestamp = block.timestamp;
         // Allocate stETH raffle rewards to winning user
-        // s_userDeposit[winner] += s_stakingRewardsTotal;
-        // s_recentWinner = winner;
-        // emit PickedWinner(winner);
+        s_userDeposit[winner] += s_stakingRewardsTotal;
+        s_recentWinner = winner;
+        emit PickedWinner(winner);
     }
 
     // This function can be called after updating the s_userDeposit for a deposit operation
@@ -239,5 +271,72 @@ contract RafflePool is VRFConsumerBaseV2 {
             s_players.pop();
             delete s_playerIndex[_user];
         }
+    }
+
+    function calculateTwab(address userAddress, uint256 s_startTime, uint256 s_endTime)
+        internal
+        view
+        returns (uint256)
+    {
+        Twab[] storage twabs; // pointer to storage-based arrays
+        if (userAddress == address(0)) {
+            twabs = s_totalDepositTwabs;
+        } else {
+            twabs = s_userTwabs[userAddress];
+        }
+
+        uint256 precedingIndex = findPrecedingTimestampIndex(twabs, s_startTime);
+
+        uint256 balanceCumulative = 0;
+        uint256 prevTimestamp = s_startTime;
+        uint256 prevBalance = precedingIndex == type(uint256).max ? 0 : twabs[precedingIndex].balance;
+
+        for (uint256 i = precedingIndex + 1; i < twabs.length; i++) {
+            if (twabs[i].timestamp > s_endTime) {
+                break;
+            }
+
+            uint256 duration = uint256(twabs[i].timestamp - prevTimestamp);
+            balanceCumulative += prevBalance * duration;
+
+            prevTimestamp = twabs[i].timestamp;
+            prevBalance = twabs[i].balance;
+        }
+
+        uint256 finalDuration = uint256(s_endTime - prevTimestamp);
+        balanceCumulative += prevBalance * finalDuration;
+
+        return balanceCumulative / (s_endTime - s_startTime);
+    }
+
+    function findPrecedingTimestampIndex(Twab[] storage twabs, uint256 s_lastTimeStamp)
+        internal
+        view
+        returns (uint256)
+    {
+        if (twabs.length == 0) {
+            return type(uint256).max; // Empty array case
+        }
+
+        uint256 start = 0;
+        uint256 end = twabs.length - 1;
+        uint256 mid;
+        uint256 result = type(uint256).max; // Initialising with "not found" value
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+
+            if (twabs[mid].timestamp <= s_lastTimeStamp) {
+                result = mid; // Found a potential preceding timestamp
+                start = mid + 1;
+            } else {
+                if (mid == 0) {
+                    break; // Prevents underflow
+                }
+                end = mid - 1;
+            }
+        }
+
+        return result;
     }
 }
