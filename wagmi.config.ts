ea233fd0c8ba638aac5edf35f131cdadc3f1dc73
Josh <joshhellings@outlook.com>
2023-08-22 14:46:35 +0100

    Add sol files to exclude during generation

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -6,6 +6,30 @@ export default defineConfig({
   out: 'src/generated.ts',
   plugins: [
     foundry({
+      exclude: [
+        'DevOpsTools.sol/**',
+        'mocks/**',
+        'Fork.t.sol/**',
+        'VRFCoordinatorV2Mock.sol/**',
+        'ERC20.sol/**',
+        // the following patterns are excluded by default
+        'Common.sol/**',
+        'Components.sol/**',
+        'Script.sol/**',
+        'StdAssertions.sol/**',
+        'StdError.sol/**',
+        'StdCheats.sol/**',
+        'StdMath.sol/**',
+        'StdJson.sol/**',
+        'StdStorage.sol/**',
+        'StdUtils.sol/**',
+        'Vm.sol/**',
+        'console.sol/**',
+        'console2.sol/**',
+        'test.sol/**',
+        '**.s.sol/*.json',
+        '**.t.sol/*.json',
+      ],
       forge: {
         // `wagmi generate` was failing at `forge build` step by trying to specify --root in build command. 
         // So compiler failed in trying to find IERC20.sol file
@@ -14,8 +38,11 @@ export default defineConfig({
       },
       deployments: {
         StakePool: {
-          [chains.goerli.id]: '0x69E9b9c9756e79bdc6064D3349DC06a9AF95287A',
+          [chains.goerli.id]: '0xf53C6016781F3F3ec9E40495CC21Ad0E1608292D',
         },
+        RafflePool: {
+          [chains.goerli.id]: '0xc797FcFD56152b5cDF16C87284eE3922cdfB1A5A',
+        }
       },
       project: './contracts',
     }),
