ec95976987fcbf3ea17ca25029d75dfebda463d1
Josh <joshhellings@outlook.com>
2023-08-24 16:45:30 +0100

    Add old StakePool deploy

--- contracts/script/DeployStakePool.s.sol
+++ contracts/script/DeployStakePool.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {StakePool} from "../src/StakePool.sol";
