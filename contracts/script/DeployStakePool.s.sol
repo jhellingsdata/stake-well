643af20d41b25186b55f9d13bec4b021f8352b67
Josh <joshhellings@outlook.com>
2023-08-30 13:53:26 +0100

    Update old StakePool deploy

--- contracts/script/DeployStakePool.s.sol
+++ contracts/script/DeployStakePool.s.sol
@@ -7,7 +7,7 @@ import {StakePool} from "../src/StakePool.sol";
 contract DeployStakePool is Script {
     function run() external returns (StakePool) {
         vm.startBroadcast();
-        StakePool stakePool = new StakePool();
+        StakePool stakePool = new StakePool(0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F);
         vm.stopBroadcast();
         return stakePool;
     }
