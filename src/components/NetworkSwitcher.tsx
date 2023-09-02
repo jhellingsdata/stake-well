52223e3fe74149bc1f90855e4b6258e1b44cc0c8
Josh <joshhellings@outlook.com>
2023-09-02 14:08:03 +0100

    Improve Network switcher with custom button

--- src/components/NetworkSwitcher.tsx
+++ src/components/NetworkSwitcher.tsx
@@ -1,34 +1,65 @@
+// 'use client'
+
+// import { BaseError } from 'viem'
+// import { useNetwork, useSwitchNetwork } from 'wagmi'
+// import CustomButton from './CustomButton';
+
+// export function NetworkSwitcher() {
+//   const { chain } = useNetwork()
+//   const { chains, error, isLoading, pendingChainId, switchNetwork } =
+//     useSwitchNetwork()
+
+//   return (
+//     <div>
+//       <div>
+//         {chain?.name ?? chain?.id}
+//         {chain?.unsupported && ' (unsupported)'}
+//       </div>
+
+// {switchNetwork && (
+//   <div>
+//     {chains.map((x) =>
+//       x.id === chain?.id ? null : (
+//         <button key={x.id} onClick={() => switchNetwork(x.id)}>
+//           {x.name}
+//           {isLoading && x.id === pendingChainId && ' (switching)'}
+//         </button>
+//       ),
+//     )}
+//   </div>
+// )}
+
+//       <div>{error && (error as BaseError).shortMessage}</div>
+//     </div>
+//   )
+// }
+
 'use client'
 
 import { BaseError } from 'viem'
 import { useNetwork, useSwitchNetwork } from 'wagmi'
+import CustomButton from './CustomButton';
 
 export function NetworkSwitcher() {
-  const { chain } = useNetwork()
-  const { chains, error, isLoading, pendingChainId, switchNetwork } =
-    useSwitchNetwork()
+  const { chain } = useNetwork();
+  const { chains, error, isLoading, pendingChainId, switchNetwork } = useSwitchNetwork();
 
   return (
     <div>
-      <div>
-        Connected to {chain?.name ?? chain?.id}
-        {chain?.unsupported && ' (unsupported)'}
-      </div>
-
       {switchNetwork && (
         <div>
           {chains.map((x) =>
             x.id === chain?.id ? null : (
-              <button key={x.id} onClick={() => switchNetwork(x.id)}>
-                {x.name}
-                {isLoading && x.id === pendingChainId && ' (switching)'}
-              </button>
+              <><CustomButton
+                title={isLoading ? 'Check Wallet...' : `Switch to ${x.name}`}
+                containerStyles='tracking-wide rounded-3xl px-4 bg-red-500 text-normal mr-4 ml-4'
+                handleClick={() => switchNetwork(x.id)} />
+              </>
             ),
           )}
         </div>
       )}
-
-      <div>{error && (error as BaseError).shortMessage}</div>
     </div>
-  )
+  );
 }
+
