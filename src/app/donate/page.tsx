d81aa3a385db5a26d2f4274e5a420eaef16bcafb
Josh <joshhellings@outlook.com>
2023-09-04 13:53:28 +0100

    Cleaning

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -10,18 +10,7 @@ export function Page() {
             <div className="mx-auto pt-10">
                 <CreateCampaign />
             </div>
-            <div className="hero">
-                <div className="flex-1 pt-36 padding-x">
-                    <h1 className="hero__title">
-                        A no-loss protocol to expand the use of Ethereum staking rewards.
-                    </h1>
 
-                    <p className="hero__subtitle">
-                        Support your favourite projects or win pooled rewards without risking your funds.
-                    </p>
-
-                </div>
-            </div>
         </>
     )
 }
--- src/app/raffle/page.tsx
+++ src/app/raffle/page.tsx
@@ -1,9 +1,13 @@
+import { PoolUserBalance } from '@/components/PoolUserBalance'
 import RaffleDetails from '@/components/RaffleDetails'
+import { PoolTransaction } from '@/components/RaffleTransaction'
 
 export function Page() {
     return (
         <>
             <RaffleDetails />
+            <PoolUserBalance />
+            {/* <PoolTransaction /> */}
         </>
     )
 }
