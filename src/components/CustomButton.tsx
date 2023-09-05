458a87b706819bc91801139c46782add4fd3c58b
Josh <joshhellings@outlook.com>
2023-09-05 14:32:40 +0100

    Extend custom btn component with mouse events

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,19 +1,19 @@
 'use client';
 
 import { CustomButtonProps } from '../types';
-import Image from 'next/image';
-
 // Note: by default, all components in Next.js are serverside rendered.
 // This button will be a client-side rendered component 
 // (usually the case when there are browser-side actions such as clicks or using hooks). 
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, disabled, containerStyles, handleClick, handleMouseEnter, handleMouseLeave }: CustomButtonProps) => {
     return (
         <button
-            disabled={false}
+            disabled={disabled}
             type={"button"}
             className={`custom-btn ${containerStyles}`}
             onClick={handleClick}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
         >
             <span className={`flex-1`}>
                 {title}
